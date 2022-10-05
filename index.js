e916484ed04a5aebf1b5e49006e702c478fb84ec
Harrison <harriwilson5534@gmail.com>
2022-10-05 14:45:54 -0400

    lotso good stuff here

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    encounterImage.src = "https://static.3dbaza.com/original_models/258923/7282411cc7fb4d55bfffe03a.jpg"
+    encounterName.textContent = "SELECT YOUR CHARACTER"
     characterSelector()
 })
 
@@ -24,7 +26,18 @@ let dexterity = 0
 let charisma = 0
 let wisdom = 0
 let playerHP = 0
+let playerID = 0
+let characterClass = ""
 let fightCounter = 0 //keeps track of the number of fights before the final boss can be available
+
+//PLAYER STAT HTML ELEMENTS
+let displayClass = document.getElementById('class')
+let displayHealth = document.getElementById('health')
+let displayAttack = document.getElementById('attackMod')
+let displaySneak = document.getElementById('dexMod')
+let displayCharm = document.getElementById('charMod')
+let displayRun = document.getElementById('runMod')
+let displayWisdom = document.getElementById('wisMod')
 
 //MONSTER VARIABLES
 let monsterHealth = 0;
@@ -41,7 +54,7 @@ sneakButton.textContent = "Sneak"
 function sneakByMonster() {
     sneakButton.addEventListener('click', e => {
         let sneakCheck = rollDice(20, dexterity)
-        console.log(sneakCheck)
+        //console.log(sneakCheck)
             if (sneakCheck >= monsterPerception) {
                 battleText.textContent = "You snuck away!"
                 storyHeadline.append(nextButtonToMountain)
@@ -68,8 +81,10 @@ function attackMonster() {
         let attackCheck = rollDice(20, strength)
         if (attackCheck >= monsterArmor) {
             let damage = rollDice(8, strength)
+            console.log(damage)
             monsterHealth = monsterHealth - damage
             console.log(monsterHealth)
+            //console.log(monsterHealth)
             battleText.textContent = `You dealt ${damage} damage to the monster. It has ${monsterHealth} hit points left!`
             if (monsterHealth <= 0) {
                 removeButtons()
@@ -78,6 +93,7 @@ function attackMonster() {
         } else if (attackCheck < monsterArmor) {
             let monsterAttack = rollDice(8, 5)
             playerHP = playerHP - monsterAttack
+            console.log(playerHP)
             battleText.textContent = `Your attack missed! The monster counter attacks and deals ${monsterAttack} damage to you. You have ${playerHP} hit points left!`
             if (playerHP <= 0) {
                 removeButtons()
@@ -94,7 +110,7 @@ charmButton.textContent = "Charm"
 function charmMonster() {
     charmButton.addEventListener('click', e => {
         let charmCheck = rollDice(20, charisma)
-        console.log(charmCheck)
+        //console.log(charmCheck)
         if (charmCheck >= monsterCharmResistance) {
             battleText.textContent = "The monster was seduced by you. Congrats. You have a monster lover now, you weirdo."
             storyHeadline.append(nextButtonToMountain)
@@ -118,12 +134,12 @@ function runFromMonster(){
     runButton.addEventListener('click', e => {
     let runCheck = rollDice(20, strength)
         if (runCheck >= monsterStrength) {
-            battleText.textContent = "You ran under the troll's legs, and make a beeline for the mountain!"
+            battleText.textContent = "You ran under the monster's legs, and make a beeline for the exit!"
             storyHeadline.append(nextButtonToMountain)
             removeButtons()
         } else if (runCheck < monsterStrength) {
             playerHP -= 5
-            console.log(playerHP)
+            //console.log(playerHP)
             battleText.textContent = `You tried to bolt around the monster, but he took a swing at you and hits you for 5 damage. Your current health is ${playerHP}`
             if (playerHP <= 0) {
                 removeButtons()
@@ -139,6 +155,13 @@ function characterSelector() {
     let confirmCharacterButton = document.createElement('button')
     confirmCharacterButton.addEventListener('click', e => {
         tavernStart()
+        displayClass.textContent = `Class: ${characterClass}`
+        displayHealth.textContent = `Health: ${playerHP}`
+        displayAttack.textContent = `Attack Modifier: ${strength}`
+        displaySneak.textContent = `Sneak Modifier: ${dexterity}`
+        displayCharm.textContent = `Charm Modifier: ${charisma}`
+        displayRun.textContent = `Run Modifier: ${strength}`
+        displayWisdom.textContent = `Wisdom Modifier: ${wisdom}`
         confirmCharacterButton.remove()
         removeSpan()
     })
@@ -164,12 +187,13 @@ function characterSelector() {
                 <p>Wisdom Modifier: ${character.wisdom}</p>
                 <p>Charisma Modifier: ${character.char}</p>
                 <p>Health Total: ${character.health}</p>`
-                
+                characterClass = character.name
+                playerID = character.id
                 strength = character.str
                 dexterity = character.dex
                 charisma = character.char
                 wisdom = character.wisdom
-                playerHP = character.health 
+                playerHP = character.health
                 })
             })
           }
@@ -242,44 +266,48 @@ function tavernStart() {
 function mountainHubWorld() {
     battleText.textContent = ""
     fightCounter+= 1
-    console.log(fightCounter)
+    //console.log(fightCounter)
     if (fightCounter === 1) {
         let nextButton = document.createElement('button')
         nextButton.textContent = "NEXT"
         nextButton.addEventListener('click', () => {
-        clownFight()
-        nextButton.remove()
+            clownFight()
+            nextButton.remove()
         })
         storyHeadline.append(nextButton)
     } else if (fightCounter === 2) {
         let nextButton = document.createElement('button')
         nextButton.textContent = "NEXT"
         nextButton.addEventListener('click', () => {
-        sleepyGuyFight()
-        nextButton.remove()
+            owlBearFight()
+            nextButton.remove()
         })
         storyHeadline.append(nextButton)
     } else if (fightCounter === 3) {
         let nextButton = document.createElement('button')
         nextButton.textContent = "NEXT"
         nextButton.addEventListener('click', () => {
-        travelToFinalBoss()
-        nextButton.remove()
+            travelToFinalBoss()
+            nextButton.remove()
         })
         storyHeadline.append(nextButton)
     }
     encounterName.textContent = "You defeated that monster"
-    storyText.textContent = "It was a hard fought battle, but you emerged victorious, and now it's time to press on! Your HP has been restored after a moment of rest. Move on to your next combatant:"
+    storyText.textContent = "Your HP has been restored after a moment of rest. Move on to your next combatant:"
     encounterImage.src = "https://static.wikia.nocookie.net/emerald-isles/images/a/a7/Mountain_Travel.jpg/revision/latest?cb=20180209151032"
     battleText.textContent = ""
-    playerHP = 20; // must edit to reset health for each character 
+    fetch("http://localhost:3000/classes") // must edit to reset health for each character 
+    .then(resp => resp.json())
+    .then(data => {
+        playerHP = data[playerID - 1].health
+    })
 }
 
 //MONSTER FIGHTS
 
 // 1: TROLL FIGHT
 function trollFight() {
-    encounterImage.src = "https://i.ytimg.com/vi/OAvwVzODByQ/hqdefault.jpg"
+    encounterImage.src = "https://qph.cf2.quoracdn.net/main-qimg-bcc2229353e9b73ae49393f8d92b8470-lq"
     storyText.textContent = "On your route to the mountain, you must cross a river. You must first cross a bridge, guarded by a troll. What will you do to get around the troll?"
     encounterName.textContent = "A TROLL ON THE BRIDGE"
     monsterHealth = 30
@@ -305,55 +333,63 @@ function trollFight() {
 function clownFight() {
     encounterImage.src = "https://m.media-amazon.com/images/M/MV5BMWFkY2MzMGUtMmMxYS00ZWU2LWE0NjMtYTcxNTNlYjIyODdjXkEyXkFqcGdeQXRzdGFzaWVr._V1_QL75_UX500_CR0,6,500,281_.jpg"
     storyText.textContent = "Oh no, there's a freaky clown blocking your path. What are you going to do?"
-    encounterName.textContent = "CLOWN IN YOUR FACE"
-    monsterHealth = 15
+    encounterName.textContent = "A KILLER CELESTIAL CLOWN BLOCKS YOUR PATH"
+    monsterHealth = 20
     monsterArmor = 10
     monsterPerception = 17
     monsterCharmResistance = 17
     monsterStrength = 13
+    console.log(monsterHealth)
     //Sneak Option
-    sneakByMonster();
     storyHeadline.append(sneakButton)
     // Attack option
-    attackMonster();
     storyHeadline.append(attackButton)
     // Charm option
-    charmMonster()
     storyHeadline.append(charmButton)
     //Run option
-    runFromMonster()
     storyHeadline.append(runButton)
 }
 
 // 3: SLEEPY GUY FIGHT
-function sleepyGuyFight() {
-    encounterImage.src = "https://cdn.mos.cms.futurecdn.net/YMzrA4GXpKFdn8Ez2GH2JX.jpg"
-    storyText.textContent = 'A man is asleep in his bed. He looks at you with a look of horror. "What did I do wrong?" he asks'
-    encounterName.textContent = "WHO'S THE REAL BAD GUY HERE?"
-    monsterHealth = 1
-    monsterArmor = 1
-    monsterPerception = 1
-    monsterCharmResistance = 1
-    monsterStrength = 1
+function owlBearFight() {
+    encounterImage.src = "https://i.pinimg.com/originals/37/d8/54/37d854318335b618b238510631d65890.jpg"
+    storyText.textContent = 'When venturing toward the mountain, you encounter a hungry Owl Bear! It stares you down, and after a tense moment, it charges you. What will you do?'
+    encounterName.textContent = "AN OWL BEAR APPEARS"
+    monsterHealth = 30
+    monsterArmor = 16
+    monsterPerception = 16
+    monsterCharmResistance = 12
+    monsterStrength = 19
+    console.log(monsterHealth)
     //Sneak Option
-    sneakByMonster();
     storyHeadline.append(sneakButton)
     // Attack option
-    attackMonster();
     storyHeadline.append(attackButton)
     // Charm option
-    charmMonster()
     storyHeadline.append(charmButton)
     //Run option
-    runFromMonster()
     storyHeadline.append(runButton)
 }
 
-// 4: FINAL BOSS FIGHT
+// 7: FINAL BOSS FIGHT
 function travelToFinalBoss() {
     encounterImage.src = "https://static.scientificamerican.com/blogs/cache/file/9C720750-2370-44E1-ACE260270EB7EA75_source.jpg?w=590&h=800&FAECB764-BEBE-4D3D-A7705363D7C3BF12"
-    storyText.textContent = "A big ol dragon gonna eat your face"
-    encounterName.textContent = "rawrXD"
+    storyText.textContent = "RAWR XD"
+    encounterName.textContent = "A big ol dragon gonna eat your face"
+    monsterHealth = 55
+    monsterArmor = 19
+    monsterPerception = 23
+    monsterCharmResistance = 23
+    monsterStrength = 23
+    console.log(monsterHealth)
+    //Sneak Option
+    storyHeadline.append(sneakButton)
+    // Attack option
+    storyHeadline.append(attackButton)
+    // Charm option
+    storyHeadline.append(charmButton)
+    //Run option
+    storyHeadline.append(runButton)
 }
 
 // NEXT STEPS
